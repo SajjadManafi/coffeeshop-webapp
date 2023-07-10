ffe286f58f2593df57f7ea9c757c2a6063465f7a
alisadigh <75201484+alisadigh@users.noreply.github.com>
2023-07-10 15:06:16 +0330

    tools that we need

---
+++ tools/tools.go
@@ -0,0 +1,10 @@
+package tools
+
+import (
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "github.com/kyleconroy/sqlc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
+
+/* go build tools */
