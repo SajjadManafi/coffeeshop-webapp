6008a818b83698b6fa1fb4c21788afd20f02fa57
alisadigh <75201484+alisadigh@users.noreply.github.com>
2023-07-10 13:59:10 +0330

    kitchen DB migration sql files

---
+++ db/migrations/000003_init_kitchendb.up.sql
@@ -0,0 +1,27 @@
+START TRANSACTION;
+
+CREATE SCHEMA IF NOT EXISTS "kitchen";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE
+    kitchen.kitchen_orders (
+        id uuid NOT NULL DEFAULT (uuid_generate_v4()),
+        order_id uuid NOT NULL,
+        item_type integer NOT NULL,
+        item_name text NOT NULL,
+        time_up timestamp
+        with
+            time zone NOT NULL,
+            created timestamp
+        with
+            time zone NOT NULL DEFAULT (now()),
+            updated timestamp
+        with
+            time zone NULL,
+            CONSTRAINT pk_kitchen_orders PRIMARY KEY (id)
+    );
+
+CREATE UNIQUE INDEX ix_kitchen_orders_id ON kitchen.kitchen_orders (id);
+
+COMMIT;
