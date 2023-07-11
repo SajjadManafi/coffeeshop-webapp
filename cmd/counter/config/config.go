1ba0f2e413ca9b0282ca01db18bb88b9a6a60516
alisadigh <75201484+alisadigh@users.noreply.github.com>
2023-07-11 19:59:47 +0330

    counter service config

---
+++ cmd/counter/config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	configs "github.com/uucoffeeshop/coffeeshop-application/pkg/config"
+)
+
+type (
+	Config struct {
+		configs.App   `yaml:"app"`
+		configs.HTTP  `yaml:"http"`
+		configs.Log   `yaml:"logger"`
+		PG            `yaml:"postgres"`
+		RabbitMQ      `yaml:"rabbitmq"`
+		ProductClient `yaml:"product_client"`
+	}
+
+	PG struct {
+		PoolMax int    `env-required:"true" yaml:"pool_max" env:"PG_POOL_MAX"`
+		DsnURL  string `env-required:"true" yaml:"dsn_url" env:"PG_DSN_URL"`
+	}
+
+	RabbitMQ struct {
+		URL string `env-required:"true" yaml:"url" env:"RABBITMQ_URL"`
+	}
+
+	ProductClient struct {
+		URL string `env-required:"true" yaml:"url" env:"PRODUCT_CLIENT_URL"`
+	}
+)
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// debug
+	fmt.Println("config path: " + dir)
+
+	err = cleanenv.ReadConfig(dir+"/config.yml", cfg)
+	if err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	err = cleanenv.ReadEnv(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
