c958ebfe4eb30f4bc56da0a1ea51796319dc6467
alisadigh <75201484+alisadigh@users.noreply.github.com>
2023-07-11 18:22:25 +0330

    implement Kitchen service app and use wire for generate dependency injection

---
+++ internal/kitchen/app/app.go
@@ -0,0 +1,83 @@
+package app
+
+import (
+	"context"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/uucoffeeshop/coffeeshop-application/cmd/kitchen/config"
+	"github.com/uucoffeeshop/coffeeshop-application/internal/kitchen/eventhandlers"
+	"github.com/uucoffeeshop/coffeeshop-application/internal/pkg/event"
+	"github.com/uucoffeeshop/coffeeshop-application/pkg/postgres"
+	pkgConsumer "github.com/uucoffeeshop/coffeeshop-application/pkg/rabbitmq/consumer"
+	pkgPublisher "github.com/uucoffeeshop/coffeeshop-application/pkg/rabbitmq/publisher"
+	"golang.org/x/exp/slog"
+)
+
+type App struct {
+	Cfg *config.Config
+
+	PG       postgres.DBEngine
+	AMQPConn *amqp.Connection
+
+	CounterOrderPub pkgPublisher.EventPublisher
+	Consumer        pkgConsumer.EventConsumer
+
+	handler eventhandlers.KitchenOrderedEventHandler
+}
+
+func New(
+	cfg *config.Config,
+	pg postgres.DBEngine,
+	amqpConn *amqp.Connection,
+	counterOrderPub pkgPublisher.EventPublisher,
+	consumer pkgConsumer.EventConsumer,
+	handler eventhandlers.KitchenOrderedEventHandler,
+) *App {
+	return &App{
+		Cfg:      cfg,
+		PG:       pg,
+		AMQPConn: amqpConn,
+
+		CounterOrderPub: counterOrderPub,
+		Consumer:        consumer,
+
+		handler: handler,
+	}
+}
+
+func (c *App) Worker(ctx context.Context, messages <-chan amqp.Delivery) {
+	for delivery := range messages {
+		slog.Info("processDeliveries", "delivery_tag", delivery.DeliveryTag)
+		slog.Info("received", "delivery_type", delivery.Type)
+
+		switch delivery.Type {
+		case "kitchen-order-created":
+			var payload event.KitchenOrdered
+			err := json.Unmarshal(delivery.Body, &payload)
+
+			if err != nil {
+				slog.Error("failed to Unmarshal message", err)
+			}
+
+			err = c.handler.Handle(ctx, payload)
+
+			if err != nil {
+				if err = delivery.Reject(false); err != nil {
+					slog.Error("failed to delivery.Reject", err)
+				}
+
+				slog.Error("failed to process delivery", err)
+			} else {
+				err = delivery.Ack(false)
+				if err != nil {
+					slog.Error("failed to acknowledge delivery", err)
+				}
+			}
+		default:
+			slog.Info("default")
+		}
+	}
+
+	slog.Info("deliveries channel closed")
+}
---
+++ internal/kitchen/app/wire.go
@@ -0,0 +1,46 @@
+//go:build wireinject
+// +build wireinject
+
+package app
+
+import (
+	"github.com/google/wire"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/uucoffeeshop/coffeeshop-application/cmd/kitchen/config"
+	"github.com/uucoffeeshop/coffeeshop-application/internal/kitchen/eventhandlers"
+	"github.com/uucoffeeshop/coffeeshop-application/pkg/postgres"
+	"github.com/uucoffeeshop/coffeeshop-application/pkg/rabbitmq"
+	pkgConsumer "github.com/uucoffeeshop/coffeeshop-application/pkg/rabbitmq/consumer"
+	pkgPublisher "github.com/uucoffeeshop/coffeeshop-application/pkg/rabbitmq/publisher"
+)
+
+func InitApp(
+	cfg *config.Config,
+	dbConnStr postgres.DBConnString,
+	rabbitMQConnStr rabbitmq.RabbitMQConnStr,
+) (*App, func(), error) {
+	panic(wire.Build(
+		New,
+		dbEngineFunc,
+		rabbitMQFunc,
+		pkgPublisher.EventPublisherSet,
+		pkgConsumer.EventConsumerSet,
+		eventhandlers.KitchenOrderedEventHandlerSet,
+	))
+}
+
+func dbEngineFunc(url postgres.DBConnString) (postgres.DBEngine, func(), error) {
+	db, err := postgres.NewPostgresDB(url)
+	if err != nil {
+		return nil, nil, err
+	}
+	return db, func() { db.Close() }, nil
+}
+
+func rabbitMQFunc(url rabbitmq.RabbitMQConnStr) (*amqp.Connection, func(), error) {
+	conn, err := rabbitmq.NewRabbitMQConn(url)
+	if err != nil {
+		return nil, nil, err
+	}
+	return conn, func() { conn.Close() }, nil
+}
