049a0a444c54f2ceb87a95fc3cbd14cd93c927b1
Sajjad Manafi <80972243+SajjadManafi@users.noreply.github.com>
2023-07-11 17:35:18 +0000

    web service main file (implement file render)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/golang/glog"
+	"github.com/labstack/echo/v4"
+)
+
+//go:embed app
+var embededFiles embed.FS
+
+func getFileSystem(useOS bool) http.FileSystem {
+	if useOS {
+		log.Print("using live mode")
+
+		return http.FS(os.DirFS("app"))
+	}
+
+	log.Print("using embed mode")
+
+	fsys, err := fs.Sub(embededFiles, "app")
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
+}
+
+type UrlModel struct {
+	Url string `json:"url"`
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	reverseProxyURL, ok := os.LookupEnv("REVERSE_PROXY_URL")
+	if !ok || reverseProxyURL == "" {
+		glog.Fatalf("web: environment variable not declared: reverseProxyURL")
+	}
+
+	webPort, ok := os.LookupEnv("WEB_PORT")
+	if !ok || webPort == "" {
+		glog.Fatalf("web: environment variable not declared: webPort")
+	}
+
+	e := echo.New()
+
+	useOS := len(os.Args) > 1 && os.Args[1] == "live"
+	assetHandler := http.FileServer(getFileSystem(useOS))
+	e.GET("/", echo.WrapHandler(assetHandler))
+	e.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", assetHandler)))
+	e.GET("/reverse-proxy-url", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, UrlModel{Url: reverseProxyURL})
+	})
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", webPort)))
 }
--- go.mod
+++ go.mod
@@ -13,7 +13,13 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/genproto v0.0.0-20230526203410-71b5a4ffd15e // indirect
@@ -28,6 +34,7 @@ require (
 	github.com/golang/glog v1.1.1
 	github.com/google/wire v0.5.0
 	github.com/ilyakaznacheev/cleanenv v1.4.2
+	github.com/labstack/echo/v4 v4.10.2
 	github.com/lib/pq v1.10.9
 	github.com/pkg/errors v0.9.1
 	github.com/rabbitmq/amqp091-go v1.8.1
