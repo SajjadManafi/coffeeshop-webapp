903ffc6ce7cd3a0ee28d468d6ec29bf702da45ea
SajjdManafi <sajadmanafi8@gmail.com>
2023-07-11 17:03:06 +0330

    implement Barista service app and use wire for generate dependency injection

--- go.mod
+++ go.mod
@@ -8,13 +8,17 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
 
 require (
 	github.com/google/wire v0.5.0
+	github.com/ilyakaznacheev/cleanenv v1.4.2
 	github.com/pkg/errors v0.9.1
 	github.com/rabbitmq/amqp091-go v1.8.1
 	github.com/stretchr/testify v1.8.4
---
+++ internal/barista/app/app.go
@@ -0,0 +1,83 @@
+package app
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/uucoffeeshop/coffeeshop-application/cmd/barista/config"
+	"github.com/uucoffeeshop/coffeeshop-application/internal/barista/eventhandlers"
+	"github.com/uucoffeeshop/coffeeshop-application/internal/pkg/event"
+	"github.com/uucoffeeshop/coffeeshop-application/pkg/postgres"
+	pkgConsumer "github.com/uucoffeeshop/coffeeshop-application/pkg/rabbitmq/consumer"
+	pkgPublisher "github.com/uucoffeeshop/coffeeshop-application/pkg/rabbitmq/publisher"
+	"golang.org/x/exp/slog"
+)
+
+type App struct {
+	Cfg      *config.Config
+	PG       postgres.DBEngine
+	AMQPConn *amqp.Connection
+
+	CounterOrderPub pkgPublisher.EventPublisher
+	Consumer        pkgConsumer.EventConsumer
+
+	handler eventhandlers.BaristaOrderedEventHandler
+}
+
+func New(
+	cfg *config.Config,
+	pg postgres.DBEngine,
+	amqpConn *amqp.Connection,
+	counterOrderPub pkgPublisher.EventPublisher,
+	consumer pkgConsumer.EventConsumer,
+	handler eventhandlers.BaristaOrderedEventHandler,
+) *App {
+	return &App{
+		Cfg:      cfg,
+		PG:       pg,
+		AMQPConn: amqpConn,
+
+		CounterOrderPub: counterOrderPub,
+		Consumer:        consumer,
+
+		handler: handler,
+	}
+}
+
+func (c *App) Worker(ctx context.Context, messages <-chan amqp091.Delivery) {
+	for delivery := range messages {
+		slog.Info("processDeliveries", "delivery_tag", delivery.DeliveryTag)
+		slog.Info("received", "delivery_type", delivery.Type)
+
+		switch delivery.Type {
+		case "barista-order-created":
+			var payload event.BaristaOrdered
+
+			err := json.Unmarshal(delivery.Body, &payload)
+			if err != nil {
+				slog.Error("failed to Unmarshal", err)
+			}
+
+			err = c.handler.Handle(ctx, payload)
+
+			if err != nil {
+				if err = delivery.Reject(false); err != nil {
+					slog.Error("failed to delivery.Reject", err)
+				}
+
+				slog.Error("failed to process delivery", err)
+			} else {
+				err = delivery.Ack(false)
+				if err != nil {
+					slog.Error("failed to acknowledge delivery", err)
+				}
+			}
+		default:
+			slog.Info("default")
+		}
+	}
+
+	slog.Info("Deliveries channel closed")
+}
